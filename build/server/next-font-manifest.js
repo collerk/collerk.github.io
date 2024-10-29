e58c70782dde933d885de882b02b5ee074392304
zameerkhan <200546819@student.georgianc.on.ca>
2024-10-29 17:46:15 -0400

    [Add] new template

---
+++ build/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/zameerkhan/Documents/projects/collerk.github.io/src/app/layout":["static/media/f7040064460a9b21-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
